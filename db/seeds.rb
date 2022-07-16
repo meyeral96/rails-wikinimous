642ae19a35b214e695c14d50d5cb77ea0feea3ec
andrew meyer <andrew@meyerhome.org>
2022-07-16 16:43:53 +0100

    Add CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+require 'faker'
+
+10.times do
+  @article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::TvShows::SiliconValley.quote,
+  )
+  @article.save!
+end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
